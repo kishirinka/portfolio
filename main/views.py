90380e8758c6ff1ed394142c2ebc19a6df4df7d3
kishirinka <irmaalwizah@gmail.com>
2025-07-31 18:37:44 +0700

    push project

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'aboutme.html')
+
+def project(request):
+    return render(request, 'project.html')
